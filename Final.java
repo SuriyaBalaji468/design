a2111895fd88189f0364d29b02cd42f37f3dfe0b
SuriyaBalaji468 <95648457+SuriyaBalaji468@users.noreply.github.com>
2021-12-14 23:08:57 +0530

    hguduighdriog

---
+++ Final.java
@@ -0,0 +1,30 @@
+package com.greens;
+
+import java.util.Scanner;
+
+public class Final {
+	
+	public static void main(String[] args) {
+		
+		Scanner kl=new Scanner(System.in);
+		
+		
+		System.out.println( "enter the name");
+		String a=kl.next();
+				
+		System.out.println("enter the sentence");
+		//kl.nextLine();
+		String d=kl.nextLine();
+		
+		System.out.println("enter the number");
+		int b=kl.nextInt();
+	
+		
+
+		
+		
+		
+
+				}
+
+}
