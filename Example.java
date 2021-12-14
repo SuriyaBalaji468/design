02f25434ee5481c881ae3f5ca772e12225a701a8
SuriyaBalaji468 <95648457+SuriyaBalaji468@users.noreply.github.com>
2021-12-14 23:05:56 +0530

    ueiufhwuihio

---
+++ Example.java
@@ -0,0 +1,36 @@
+package com.greens;
+import java.util.Scanner;
+public class Example {
+	public static void main(String[] args) {
+		Scanner gb=new Scanner(System.in);
+		
+		System.out.println( "enter the name");
+		String a=gb.next();
+		
+		System.out.println("enter the sentence");
+	    gb.nextLine();
+		String b=gb.nextLine();
+		
+		System.out.println("enter a integer number");
+		int d=gb.nextInt();
+		
+		System.out.println( "float value");
+		float f=gb.nextFloat();
+		
+		System.out.println("boolean value");
+		boolean bb=gb.nextBoolean();
+		
+		System.out.println("enter short number");
+		short s=gb.nextShort();
+		
+		System.out.println("enter byte number");
+		byte o=gb.nextByte();
+		
+		
+
+		
+		
+		
+		}
+
+}
